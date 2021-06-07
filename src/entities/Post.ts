07a2cf6282bb6539fd80d67a2903752f6c949fd2
upluke <lukeforadream@gmail.com>
2021-06-06 20:22:21 -0700

    entities cli migrations

---
+++ src/entities/Post.ts
@@ -0,0 +1,19 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+
+
+@Entity()
+export class Post {
+//   @PrimaryKey()
+//   id!: number;
+
+//   @Property()
+//   createdAt: new Date();
+
+//   @Property({ onUpdate: () => new Date() }) // special hook to create a date every time update 
+//   updatedAt: new Date();
+
+//   @Property() // Property is a col in database
+//   title!: string;
+ 
+ 
+}
